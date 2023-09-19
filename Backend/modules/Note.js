00156b8cd3604a3147090e1a214707a5ada3fc31
Madhav Pandey <pmadhav279@gmail.com>
2023-09-19 10:30:08 +1000

    noteSchema is completed

---
+++ Backend/modules/Note.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+const noteSchema = new mongoose.Schema(
+    {
+user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:true
+},
+completed:{
+    type:Boolean,
+    default:false
+},
+text:{
+    type:String,
+    required:true
+},
+title:{
+    type:String,
+    required:true
+}
+},
+{
+    timestamps:true
+}
+
+
+)
+
+
+noteSchema.plugin(AutoIncrement,{
+    inc_field:"ticket",
+    id:"ticketNums",
+    start_seq:500
+})
+
+module.exports = mongoose.model("Note", noteSchema);
