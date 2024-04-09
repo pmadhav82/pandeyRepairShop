6b51da6be2d7d2533d02d0d5b09fc898e8ac31cc
Madhav Pandey <pmadhav279@gmail.com>
2024-04-09 16:35:33 +1000

    database test route is added

--- Backend/config/dbConnect.js
+++ Backend/config/dbConnect.js
@@ -4,8 +4,10 @@ const {logEvents} = require("../middleware/logger");
 
 const connectDB = async ()=>{
 
+
     try{
-await mongoose.connect(process.env.MONGO_DB_URL);
+        mongoose.connect(process.env.MONGO_DB_URL);
+        
     }catch(err){
     console.log(err.message);
     }
